89856cadaf0433f8a31201262339fb59f027802b
Chris F <Chris F>
2017-03-27 21:13:23 -0400

    Added reverse_string to javascript.

---
+++ javascript/reverse_string.js
@@ -0,0 +1,30 @@
+/* This function reverses a string using the javascript built in functions.
+ * We split the string into a character array, reverse that array, and
+ * then we bring it back together. Keeps punctuation/spaces/numbers/etc.
+ */
+var library_reverse = function(str) {
+	return str.split('').reverse().join('');
+}
+
+/* While not purely no library this changes the way that I did it in the 
+ * last example.
+ */
+var reverse_no_library = function(str) {
+	var reversed_array = [];
+	var i = str.length;
+    for (i; i >= 0; i--) {
+    	reversed_array.push(str.charAt(i));
+    }
+    return reversed_array.join('');
+}
+
+/* Adds the reverse function to the string prototype so that it can be
+ * called easier.
+ */
+String.prototype.reverse_string = function() {
+	return this.split('').reverse().join('');
+}
+
+console.log(library_reverse('test string'));
+console.log(reverse_no_library('no library'));
+console.log('prototype'.reverse_string());
